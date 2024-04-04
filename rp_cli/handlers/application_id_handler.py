a39ece46ee9ae7adc75eea6cb0ecf384978cdb73
Siavash Samadi <shsf1382@gmail.com>
2024-04-05 01:07:13 +0330

    ✨ Feat: Enhance Handlers for `int` Fields in the Config

--- rp_cli/handlers/application_id_handler.py
+++ rp_cli/handlers/application_id_handler.py
@@ -1,14 +1,32 @@
 import inquirer
-from rp_cli.config import update_config
+from inquirer.errors import ValidationError
+from rp_cli.utils.config import update_config
+from rp_cli.utils.clear_screen import clear
+from rp_cli.utils.display_message import display_message
+from rp_cli.utils.read_current_value import read_current_value
 
 def handle_application_id_update():
+    def validate_application_id(answers, value):
+        if not value.isdigit():
+            raise ValidationError("Application ID must be a number.", reason="Application ID must be a number.")
+        return True
+
+    display_message("🔑 This ID is crucial for Discord to access application details and assets.\n\n💡 Create a new application in 'Discord's Developer Portal' at https://discord.com/developers \n\n🔙 Ctrl+Shift+C: Cancel edit")
+    current_state = read_current_value("application_id", intg=True)
     questions = [
-        inquirer.Text('application_id', message="Enter new application ID")
+        inquirer.Text(
+            'application_id', 
+            message="Update application ID",
+            default=current_state,
+            validate=validate_application_id
+        )
     ]
     answers = inquirer.prompt(questions)
     if answers is None:
-        return
+        clear()
+        return display_message("👤 Operation cancelled by the user! \n\n🔙 Returning to the previous prompt...", style="error")
 
     new_application_id = answers['application_id']
-    update_config(application_id=new_application_id)
-    return "Application ID updated successfully."
+    update_config(application_id=new_application_id, intg=True)
+    clear()
+    return display_message("Application ID updated successfully 🙌🎉")
--- rp_cli/handlers/start_timestamp_handler.py
+++ rp_cli/handlers/start_timestamp_handler.py
@@ -1,14 +1,53 @@
 import inquirer
-from rp_cli.config import update_config
+from inquirer.errors import ValidationError
+from rp_cli.theme import custom_theme
+from rp_cli.utils.config import update_config
+from rp_cli.utils.clear_screen import clear
+from rp_cli.utils.display_message import display_message
+import time
+
+from rp_cli.utils.read_current_value import read_current_value
 
 def handle_start_timestamp_update():
-    questions = [
-        inquirer.Text('start_timestamp', message="Enter new start timestamp")
-    ]
-    answers = inquirer.prompt(questions)
-    if answers is None:
-        return
+    def validate_timestamp(answers, value):
+        if not value.isdigit():
+            raise ValidationError("Timestamp must be a number.", reason="Timestamp must be a number.")
+        if int(value) > int(time.time()):
+            raise ValidationError("Timestamp cannot be in the future.", reason="Timestamp cannot be in the future.")
+        return True
 
-    new_start_timestamp = answers['start_timestamp']
-    update_config(start_timestamp=new_start_timestamp)
-    return "Start timestamp updated successfully."
+    display_message("🔑 The start timestamp field represents the start time of your activity.\n\n🔙 Ctrl+Shift+C: Cancel edit")
+    questions = [
+        inquirer.List('action',
+                      message="Choose an action",
+                      choices=['Set Current Time', 'Set your own Unix timestamp'],
+                      ),
+    ]
+    answers = inquirer.prompt(questions, theme=custom_theme())
+    if answers is None:
+        clear()
+        return display_message("👤 Operation cancelled by the user! \n\n🔙 Returning to the previous prompt...", style="error")
+
+    if answers['action'] == 'Set Current Time':
+        new_start_timestamp = int(time.time())
+        update_config(start_timestamp=new_start_timestamp, intg=True)
+        clear()
+        return display_message("Start timestamp set to current time successfully 🙌🎉")
+    elif answers['action'] == 'Set your own Unix timestamp':
+        current_key = read_current_value('start_timestamp')
+        display_message(
+            "💡 A Unix timestamp is the number of seconds that have elapsed since January 1, 1970 (UTC).\n\n"
+            "💡 You can obtain the current Unix timestamp by using online tools or programming languages.\n\n"
+            "💡 For example, in Python, you can use `import time; print(int(time.time()))` to get the current timestamp."
+        )
+        questions = [
+            inquirer.Text('start_timestamp', message="Enter your Unix timestamp", default=current_key, validate=validate_timestamp)
+        ]
+        answers = inquirer.prompt(questions)
+        if answers is None:
+            clear()
+            return display_message("👤 Operation cancelled by the user! \n\n🔙 Returning to the previous prompt...", style="error")
+        new_start_timestamp = int(answers['start_timestamp'])
+        update_config(start_timestamp=new_start_timestamp, intg=True)
+        clear()
+        return display_message("Start timestamp updated successfully 🙌🎉")
